01054ace47a8d52394853c3b04f8a1b06019de62
Немсицверидзе Константин <aznauri07@gmail.com>
2022-03-08 23:08:12 +0600

    добавил функцию добавления публикации и функцию лайка

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,3 +1,19 @@
 from django.urls import path
 
+
+from instagram.views.like_view import LikeView
+from instagram.views.post_view import PostCreateView, PostDetailView, PostListView, DeleteView
+
 urlpatterns = []
+
+post_urls = [
+    path('post/add', PostCreateView.as_view(), name='create_post'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='detail_post'),
+    path('posts', PostListView.as_view(), name='list_post'),
+    path('post/<int:pk>/delete', DeleteView.as_view(), name='delete_post'),
+    path('post/like/<int:pk>', LikeView.as_view(), name='like')
+]
+
+
+urlpatterns += post_urls
+
