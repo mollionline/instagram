22393226d747264bc3c9a3f7831163466e6aa4dd
moldir <@auezova_moldir>
2022-04-08 12:02:15 +0600

    #1 Написала API для проекта Инстаграм, для постов и лайков, обновила фикстуру и папку аплудс, откорректировала недочеты

--- api_v1/views.py
+++ api_v1/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
 from rest_framework import viewsets, mixins
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
 
 from instagram.models import Post
